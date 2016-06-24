9675ccc50b0a333648d272d64f1329da24dd8732
Jack Zhang <jackzhng528@gmail.com>
2016-06-24 10:30:19 -0400

    css

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var path = require('path');
 
 var app = express();
 
+var checked = false;
+
 var options =  {
 	promiseLib: promise
 };
 
 var pgp = require('pg-promise')(options);
-
+app.use(express.static('public'));
 var db = pgp('postgres://localhost:5432/todolist');
 
 
@@ -26,29 +28,79 @@ app.set('views', path.join(__dirname,'views'));
 
 
 app.get('/',function(req,res,next){
-	db.any('SELECT * FROM list')
+	res.render('login');
+});
+
+
+app.post('/',function(req,res,next){
+	var newUser = req.body.username;
+	var newPass = req.body.password;
+	var query = "SELECT COUNT (*) FROM users WHERE username = '"+newUser+"' AND password = '"+newPass+"';";
+
+	// var user_id = req.params.user_id;
+	// console.log(user_id);
+	// expects no rows
+
+	db.any(query)
 	.then(function(data){
-		res.render('index',{ data: data });
+		data=data[0].count;
+		if(data>0){
+			checked = true;
+			res.redirect('/index/'+newUser);
+		}
+		else{
+			res.render('login',{failMess:"Invalid Username/Password"});
+		}
+	})
+	.catch(function (err){
+
+		// return next(err);
+	});
+});
+
+
+app.get('/index/:usernameref',function(req,res,next){
+	if(checked == true){
+	var userId = req.params.usernameref;
+	db.any('SELECT * FROM list WHERE usernameref =$1',userId)
+	.then(function(data){
+		res.render('index',{ data: data, userId: userId });
+	})
+	.catch(function(err){
+		return next(err);
+	});
+	}
+});
+
+
+app.post('/index/:usernameref',function(req,res,next){
+	var user = req.params.usernameref;
+	console.log(user);
+	db.none('insert into list (usernameref, todo ,description)'+
+		'values('+user+', ${list}, ${description})',
+		req.body)
+	.then(function(){
+		res.redirect('/index/'+userId);
 	})
 	.catch(function(err){
 		return next(err);
 	});
 });
 
-app.post('/',function(req,res,next){
-	db.none('INSERT INTO list(todo,description)'+
-		'values(${list},${description})',
-		req.body)
-	.then(function(){
-		res.redirect('/');
-	})
-	.catch(function (err){
-		return next(err);
-	});
+
+app.get('/index/:userId/delete', function(req,res,next){
+	var userId = req.params.userId;
+	db.none('DELETE FROM list WHERE usernameref = $1', userId)
+   .then(function() {
+   		res.redirect('/index/'+userId);	
+	 })
+	 .catch(function(err){
+	 	return next(err);
+	 });
 });
 
 
-//find description by id
+// find description by id
 app.get('/:id', function(req,res,next){
 	var listid = req.params.id;
 	db.one('SELECT * FROM list WHERE id = $1', listid)
@@ -61,4 +113,10 @@ app.get('/:id', function(req,res,next){
 });
 
 
+
+
 app.listen(3000);
+
+
+
+
--- lists.sql
+++ lists.sql
@@ -3,17 +3,25 @@ CREATE DATABASE todolist;
 
 \c todolist;
 
-
+CREATE TABLE users (
+	-- user_id	serial primary	key,
+	username text primary key,
+	password text	
+);
 
 CREATE TABLE list
 (
  id serial PRIMARY KEY,
+ usernameref text references users(username),
  todo text NOT NULL,
  description text 
 );
 
-INSERT INTO list(todo,description)
-VALUES('Wake Up',
-	'Stretch fully, make the bed and wash up!'), 
-('Eat Lunch', 'Eat as much as you can!'),
-('Eat Some More','Attempt to stuff more food in stomach.');
+INSERT INTO users(username,password) VALUES
+('islam','1234'),
+('mahedi','2345'),
+('jack','3456');
+INSERT INTO list(usernameref,todo,description) VALUES
+('islam','Wake Up','Stretch fully, make the bed and wash up!'), 
+('mahedi','Eat Lunch', 'Eat as much as you can!'),
+('jack','Eat Some More','Attempt to stuff more food in stomach.');
